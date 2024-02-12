6eaf3a4de6e9ca711c84668d2dd2aee1bf0b71fb
TRADIN <biffi@tradin.com.br>
2024-02-12 09:18:07 -0400

    Initial commit

---
+++ src/test/java/br/com/tradin/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tradin.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
